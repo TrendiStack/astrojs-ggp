880dbf69f73ccbfbc6e23caac35994b406102b7a
Terel <terel.phillips23@gmail.com>
2023-04-22 14:03:56 -0400

    -Removed poster from video element -Carousel video is not hosted on aws s3 bucket and used as the src -Deleted video and video directory

--- src/components/landing/Carousel.jsx
+++ src/components/landing/Carousel.jsx
@@ -1,50 +1,13 @@
-import slideshow from '../../assets/videos/ggpslidshow.mp4';
+import { lazy, Suspense } from 'react';
+
+const LazyComponent = lazy(() => import('./Video'));
+
 const Carousel = () => {
   return (
-    <div
-      className="
-    grid 
-    grid-cols-1 
-    md:grid-cols-2 
-    lg:h-screen"
-    >
-      <div
-        className="
-        flex 
-        items-center 
-        justify-center 
-        text-center 
-        large-text 
-        lg:mx-5
-        2xl:mx-20 
-        h-[20vh] 
-        lg:h-full
-        px-[5%] lg:px-[2%]
-      "
-      >
-        <p>
-          Satisfy your cravings with the mouthwatering flavors of Sicily, made
-          fresh every day.
-        </p>
-      </div>
-      <video
-        src={slideshow}
-        className="
-        mb-14 
-        md:mb-0 
-        h-[50vh] 
-        lg:h-full 
-        w-full 
-        object-cover 
-        order-first
-        md:order-last"
-        autoPlay
-        loop
-        muted
-        playsInline
-        preload="auto"
-        poster="https://www.ggpizza.com/wp-content/uploads/2021/04/IMG_20210409_153000.jpg"
-      />
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyComponent />
+      </Suspense>
     </div>
   );
 };
